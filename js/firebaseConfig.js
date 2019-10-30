c4068e72f2e99523367c3ff48726a352e524501d
parkjun1978 <38122407+parkjun1978@users.noreply.github.com>
2019-10-30 16:19:26 +0900

    Create firebaseConfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,14 @@
+    // Your web app's Firebase configuration
+	  var firebaseConfig = {
+      apiKey: "AIzaSyDaPX5424ptYKpz4e0VDpC1cIqGvLeyy0Q",
+      authDomain: "nrpc-d2b18.firebaseapp.com",
+      databaseURL: "https://nrpc-d2b18.firebaseio.com",
+      projectId: "nrpc-d2b18",
+      storageBucket: "nrpc-d2b18.appspot.com",
+      messagingSenderId: "1097678821322",
+      appId: "1:1097678821322:web:44a64e096e8accb42b2133",
+      measurementId: "G-1475EP4ZTM"
+	  };
+    
+	  // Initialize Firebase
+	  firebase.initializeApp(firebaseConfig);
